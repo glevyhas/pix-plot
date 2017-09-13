c7f898796df9c59c0674e0a22ec1809462ae55e4
duhaime <douglas.duhaime@gmail.com>
2017-09-13 19:16:54 -0400

    update utils for bulk processing sweeps

---
+++ utils/deploy_to_s3.py
@@ -0,0 +1,13 @@
+import os, glob
+
+files = []
+files += glob.glob('../*')
+files += glob.glob('../assets/*')
+files += glob.glob('../assets/*/*')
+
+cmd_start = 'aws s3 cp '
+cmd_mid = ' s3://yaledh-staging/tsne/'
+cmd_end = ' --acl public-read --profile yale-admin '
+
+os.system(cmd_start + ' ../index.html ' + cmd_mid + 'index.html ' + cmd_end)
+os.system(cmd_start + ' ../assets ' + cmd_mid + 'assets ' + cmd_end + ' --recursive ')
