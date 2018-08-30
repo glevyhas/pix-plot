2d9cd30d08424d94f37b71c8f697b02c44d91e75
duhaime <douglas.duhaime@gmail.com>
2018-08-30 16:49:12 -0400

    remove whitespace

--- assets/js/object-assign-polyfill.js
+++ assets/js/object-assign-polyfill.js
@@ -7,12 +7,9 @@ if (typeof Object.assign != 'function') {
       if (target == null) { // TypeError if undefined or null
         throw new TypeError('Cannot convert undefined or null to object');
       }
-
       var to = Object(target);
-
       for (var index = 1; index < arguments.length; index++) {
         var nextSource = arguments[index];
-
         if (nextSource != null) { // Skip over if undefined or null
           for (var nextKey in nextSource) {
             // Avoid bugs when hasOwnProperty is shadowed
