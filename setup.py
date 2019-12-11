ede19b55716b27f87e50b0cd8d43a4c53b0915bc
duhaime <douglas.duhaime@gmail.com>
2019-12-11 17:12:50 -0500

    0.0.39

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ for root, subdirs, files in os.walk(os.path.join('pixplot', 'web')):
 
 setup(
   name='pixplot',
-  version='0.0.38',
+  version='0.0.39',
   packages=['pixplot'],
   package_data={
     'pixplot': web,
