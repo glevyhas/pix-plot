3dd6843b134f5d28d58e60e155a17c181d0f0b40
duhaime <douglas.duhaime@gmail.com>
2020-01-13 11:49:56 -0500

    add version numbers

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(
     'umap-learn>=0.3.10',
     'yale-dhlab-rasterfairy>=1.0.3',
     'yale-dhlab-keras-preprocessing>=1.1.1',
-    'cmake',
-    'MulticoreTSNE',
+    'cmake>=3.15.3',
+    'MulticoreTSNE>=0.1',
   ],
   entry_points={
     'console_scripts': [
