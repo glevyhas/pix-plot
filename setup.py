b2c078ab193bdafd3fb4da76c672d84b19f8cc1c
duhaime <douglas.duhaime@gmail.com>
2020-09-17 15:12:52 -0400

    resize modals

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ for root, subdirs, files in os.walk(os.path.join('pixplot', 'web')):
 
 setup(
   name='pixplot',
-  version='0.0.95',
+  version='0.0.96',
   packages=['pixplot'],
   package_data={
     'pixplot': web,
